f0492d7a9beb3f78383778b6a0317601e3c36c9c
etochy <etochy@gmail.com>
2018-07-18 20:14:28 +0200

    host name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -8,6 +8,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 
-server.listen(port, () => {
+server.listen(PORT, () => {
   console.log(`Server running on ${PORT}/`);
 });
