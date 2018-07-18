204d2305832438165d6fa5c12325c7ae92b548eb
etochy <etochy@gmail.com>
2018-07-18 20:48:51 +0200

    service test

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const http = require('http');
-
 const PORT = process.env.PORT || 5000;
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+app.get('/', function (req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+ })
 
-server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}/`);
-});
+app.get('/test', function (req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('test');
+})
+
+const server = app.listen(PORT, function () {
+    console.log(`Server running on ${PORT}/`);
+})
