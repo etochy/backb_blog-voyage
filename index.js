cc259d26f00fef7dea0d9c38d826a2c72ae4c476
etochy <etochy@gmail.com>
2018-07-18 20:04:08 +0200

    premier push heroku

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -10,5 +9,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running on ${PORT}/`);
 });
