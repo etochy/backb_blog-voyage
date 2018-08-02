74da2b8dce70668d4d1714dedf5d314112a5b16d
etochy <etochy@gmail.com>
2018-08-02 17:06:21 +0200

    probleme npm?

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const express = require('express');
 var cors = require('cors');
 const app = express();
 app.use(cors());
-
 const mongoose = require("mongoose");
+const nodemailer = require('nodemailer');
 const Utilisateur = require('./models/utilisateur');
 const Article = require('./models/article');
 const bodyParser = require('body-parser');
